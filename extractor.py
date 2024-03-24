4f3ff7a161a2aae1f94ee1c97a953a0cfb528830
jalayrupera <jalayrupera13@gmail.com>
2024-03-24 18:21:11 +0530

    Add brute force matching and filteration

---
+++ extractor.py
@@ -0,0 +1,48 @@
+import cv2
+from networkx import inverse_line_graph
+import numpy as np
+
+from skimage.measure import ransac
+from skimage.transform import FundamentalMatrixTransform, EssentialMatrixTransform
+
+from constants import NUM_OF_ORBS
+
+
+class Extractor(object):
+    def __init__(self):
+        self.orb = cv2.ORB_create(NUM_OF_ORBS)
+        self.matcher = cv2.BFMatcher(cv2.NORM_HAMMING)
+        self.last = None
+
+    def extract(self, img:np.ndarray):
+        # Detection
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        feats = cv2.goodFeaturesToTrack(img, 3000, qualityLevel=0.01, minDistance=3)
+        
+        # Extraction
+        kps = [cv2.KeyPoint(x=f[0][0], y=f[0][1], size=20) for f in feats]
+        kps, desc = self.orb.compute(img, kps)
+
+        # Matching
+        ret = []
+        if self.last is not None:
+            matches = self.matcher.knnMatch(desc, self.last['desc'], k=2)
+            for m, n in matches:
+                if m.distance < 0.75*n.distance:
+                    kp1 = kps[m.queryIdx].pt
+                    kp2 = self.last['kps'][m.trainIdx].pt
+                    ret.append((kp1, kp2))
+
+        # Filter
+        if len(ret) > 0:
+            ret = np.array(ret)
+            model, inliers = ransac((ret[:, 0], ret[:, 1]),
+                                                FundamentalMatrixTransform, #TODO:Replace it with EssentialMatrix
+                                                min_samples=8,
+                                                residual_threshold=1,
+                                                max_trials=100)
+
+            ret = ret[inliers]
+
+        self.last = {'kps': kps, 'desc': desc}
+        return ret
--- slam.py
+++ slam.py
@@ -2,24 +2,26 @@ import cv2
 import numpy as np
 
 from constants import VIDEO_PATH, W, H
-from feature_extractor import FeatureExtractor
+from extractor import Extractor
 
 cap = cv2.VideoCapture(VIDEO_PATH)
-fe = FeatureExtractor()
+fe = Extractor()
 
 
 def process_frames(frame: np.ndarray):
     img: np.ndarray = cv2.resize(frame, (W, H))  # noqa: F405
 
-    kp = fe.extract(img)
+    matches = fe.extract(img)
 
-    for p in kp:
-        u, v = map(lambda x: int(round(x)), p[0])
-        cv2.circle(img, (u,v), color=(0,255,0), radius=3)
+    for pt1, pt2 in matches:
+        u1, v1 = map(lambda x: int(round(x)), pt1)
+        u2, v2 = map(lambda x: int(round(x)), pt2)
+
+        cv2.circle(img, (u1,v1), color=(0,255,0), radius=3)
+        cv2.line(img, (u1, v1), (u2,v2), color=(255, 0, 0), thickness=2)
 
     cv2.imshow("Git SLAM", img)
     cv2.waitKey(1)
-
 
 def main():
     while cap.isOpened():
@@ -29,6 +31,7 @@ def main():
             process_frames(frame)
         else:
             break
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
